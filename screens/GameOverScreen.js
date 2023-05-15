f8e5eb6791db27e517c0d9347514e25c698fa746
eunji Lee <eunjitech@gmail.com>
2023-05-15 11:35:24 +0900

    4-71: GameOverScreen Image 추가

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -7,6 +7,7 @@ import {
   ImageBackground,
   SafeAreaView,
   Button,
+  Image,
 } from "react-native";
 import theme from "../constants/colors";
 import bgImage from "../assets/gameOverBg.jpeg";
@@ -28,7 +29,7 @@ export default function GameOverScreen({ userNumber, setGameIsOver }) {
             }}
           >
             <View>
-              <View>
+              <View style={{ alignItems: "center" }}>
                 <Text
                   style={{
                     fontSize: 20,
@@ -39,18 +40,30 @@ export default function GameOverScreen({ userNumber, setGameIsOver }) {
                 >
                   Game Over!
                 </Text>
+                <View style={styles.imageBox}>
+                  <Image
+                    style={styles.image}
+                    source={require("../assets/maeg.jpeg")}
+                  />
+                </View>
               </View>
-              <View>
+              <View style={{ alignItems: "center" }}>
                 <Text
                   style={{
+                    fontFamily: "open-sans-bold",
                     fontSize: 40,
-                    textAlign: "center",
-                    fontWeight: "bold",
                     paddingVertical: 10,
                     color: theme.blueColor,
                   }}
                 >
                   The number is {userNumber}
+                </Text>
+                <Text
+                  style={{
+                    fontFamily: "open-sans",
+                  }}
+                >
+                  00번의 시도 끝에 성공!
                 </Text>
               </View>
               <Button title="Restart" onPress={() => setGameIsOver(false)} />
@@ -62,4 +75,19 @@ export default function GameOverScreen({ userNumber, setGameIsOver }) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageBox: {
+    borderRadius: 200,
+    width: 200,
+    height: 200,
+    borderWidth: 3,
+    borderColor: theme.blueColor,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+});
